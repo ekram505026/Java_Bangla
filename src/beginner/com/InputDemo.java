fee5171aaeb6cb6a772bd96479a2e2d678ceeede
Zareef Karim <ekramulkarim@yahoo.com>
2022-01-28 00:51:14 -0500

    Added spaces

--- src/beginner/com/InputDemo.java
+++ src/beginner/com/InputDemo.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class InputDemo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
 //        int number;
 //
 //        System.out.println("Enter any number: ");
 //        number = input.nextInt();
 //        System.out.println("Number = " + number);
+
         String name;
         System.out.println("Enter your name: ");
         name = input.nextLine();
