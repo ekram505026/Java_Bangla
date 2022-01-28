3e0aad58b005a03490f6c94530b3aeecc396c859
Zareef Karim <ekramulkarim@yahoo.com>
2022-01-28 00:40:21 -0500

    Add all my files

---
+++ src/beginner/com/DataType.java
@@ -0,0 +1,21 @@
+package beginner.com;
+
+public class DataType {
+    public static void main(String[] args) {
+        boolean b = true;// dynamic initialization
+        char c = 'a';
+        short s = 32677;
+        int i = 126587;
+        float f = 10.2f;
+        double d = 10.21131141;
+
+        System.out.printf("boolean b = %b\n", b);
+        System.out.printf("character c = %c\n", c);
+        System.out.printf("Short s = %d\n", s);
+        System.out.printf("integer i = %d\n", i);
+        System.out.printf("float f = %.1f\n", f);
+        System.out.printf("double d = %3f\n", d);
+
+
+    }
+}
---
+++ src/beginner/com/InputDemo.java
@@ -0,0 +1,20 @@
+package beginner.com;
+
+import java.util.Scanner;
+
+public class InputDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        int number;
+//
+//        System.out.println("Enter any number: ");
+//        number = input.nextInt();
+//        System.out.println("Number = " + number);
+        String name;
+        System.out.println("Enter your name: ");
+        name = input.nextLine();
+        System.out.println("Welcome: " + name);
+
+
+    }
+}
